219ec12e788e4cabe80ed5f51db1dacc724023f6
Gladysgong <531404376@qq.com>
2017-12-19 17:32:19 +0800

    基于关键字来爬取百度搜索的结果，并存储为json文件

--- DataSpider/DataSpider/DataSpider/pipelines.py
+++ DataSpider/DataSpider/DataSpider/pipelines.py
@@ -12,20 +12,20 @@ import itertools
 # from scrapy.xlib.pydispatch import dispatcher
 # 把字段存入json文件中
 class DataspiderPipeline(object):
-    # def __init__(self):
-    #     self.file = open("baiduSearch.json", 'w+', encoding='utf-8')
-    #
-    # def process_item(self, item, spider):
-    #     line = json.dumps(dict(item), ensure_ascii=False) + "\n"
-    #     self.file.write(line)
-    #     return item
     def __init__(self):
-        self.csvwriter = csv.writer(open("baiduSearch.csv", "wb"), delimiter=',')
-        self.csvwriter.writerow(['title', 'url', 'content', 'word'])
+        self.file = open("baiduSearch.json", 'w+', encoding='utf-8')
 
     def process_item(self, item, spider):
-        rows = zip(item['title'], item['url'], item['content'], item['word'])
-        for row in rows:
-            self.csvwriter.writerow(row)
+        line = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        self.file.write(line)
         return item
+    # def __init__(self):
+    #     self.csvwriter = csv.writer(open("baiduSearch.csv", "wb"), delimiter=',')
+    #     self.csvwriter.writerow(['title', 'url', 'content', 'word'])
+    #
+    # def process_item(self, item, spider):
+    #     rows = zip(item['title'], item['url'], item['content'], item['word'])
+    #     for row in rows:
+    #         self.csvwriter.writerow(row)
+    #     return item
 
